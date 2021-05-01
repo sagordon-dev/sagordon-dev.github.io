374c14b069e0885df89b112e799882bd0236cf13
Scott Gordon <81840601+sagordon-dev@users.noreply.github.com>
2021-05-01 17:19:43 -0400

    Add JavaScript greeter

---
+++ scripts/greeting.js
@@ -0,0 +1,16 @@
+function greetVisitor() {
+    let today = new Date();
+    let hourNow = today.getHours();
+    let greeting;
+
+    if (hourNow > 18) {
+        greeting = 'Good evening!';
+    } else if (hourNow > 12) {
+        greeting = 'Good afternoon!';
+    } else if (hourNow > 0) {
+        greeting = 'Good morning!';
+    } else {
+        greeting = 'Welcome';
+    }
+    document.getElementById('greeting').innerHTML = `${greeting}<br> Welcome to my website`;
+};
